4e0f25d7e3e73cff503cde0ab66019a96bd93405
David Moncada <david_moncada@icloud.com>
2023-06-22 15:14:00 -0700

    Basic app functionality.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native";
+import Keyboard from "./src/components/Keyboard/Keyboard";
+import Grid from "./src/components/Grid/Grid";
+
+const NUM_LETTERS: number = 5;
+const NUM_GUESSES: number = 6;
+const solution = "REACT";
 
 export default function App() {
+  const [currentGuess, setCurrentGuess] = useState<string>("");
+  const [previousGuesses, setPreviousGuesses] = useState<string[]>([]);
+
+  const onKey = (keyPressed: string) => {
+    if (currentGuess.length < NUM_LETTERS) {
+      setCurrentGuess(currentGuess + keyPressed);
+    }
+  };
+
+  const onBack = () => {
+    if (currentGuess.length > 0) {
+      setCurrentGuess(currentGuess.slice(0, -1));
+    }
+  };
+
+  const onEnter = () => {
+    if (currentGuess.length === NUM_LETTERS) {
+      setPreviousGuesses(previousGuesses.concat([currentGuess]));
+      setCurrentGuess("");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Grid
+        numCells={NUM_LETTERS}
+        numRows={NUM_GUESSES}
+        target={solution}
+        currectGuess={currentGuess}
+        previousGuesses={previousGuesses}
+      />
+
+      <Keyboard
+        target={solution}
+        guesses={previousGuesses}
+        onBack={onBack}
+        onEnter={onEnter}
+        onKey={onKey}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
