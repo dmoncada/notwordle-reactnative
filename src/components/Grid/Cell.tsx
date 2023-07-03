29c0818be5d7bc2dc219242fe1f15936feee810b
David Moncada <david_moncada@icloud.com>
2023-07-03 10:20:13 -0700

    Initial support for hints.

--- src/components/Grid/Cell.tsx
+++ src/components/Grid/Cell.tsx
@@ -8,27 +8,39 @@ const Container = styled.View<{ state: LetterState }>`
   border-radius: 6px;
   align-items: center;
   justify-content: center;
-  border-color: ${({ theme, state }) =>
-    theme.cell.border[state].toString()};
-  background-color: ${({ theme, state }) =>
-    theme.cell.background[state].toString()};
+  border-color: ${({ theme, state }) => theme.cell.border[state].toString()};
+  background-color: ${({ theme, state }) => theme.cell.background[state].toString()};
 `;
 
 const Label = styled.Text<{ state: LetterState }>`
   font-size: 24px;
   font-weight: bold;
   color: ${({ theme, state }) =>
-    state === 'wrong' || state === "inword" || state === "correct"
+    ["wrong", "inword", "correct"].includes(state)
       ? theme.textInverted.toString()
       : theme.text.toString()};
 `;
 
-const Cell = ({ letter, state }: { letter?: string; state?: LetterState }) => {
-  state = state || "unused";
+const Hint = styled.Text`
+  font-size: 12px;
+  font-weight: bold;
+  position: absolute;
+  bottom: 30px;
+  left: 34px;
+  color: ${({ theme }) => theme.textInverted.toString()};
+`;
 
+type Props = {
+  letter: string;
+  state: LetterState;
+  hint?: number;
+};
+
+const Cell = ({ letter, state, hint }: Props) => {
   return (
     <Container state={state}>
       <Label state={state}>{letter}</Label>
+      {hint > 1 ? <Hint>{hint}</Hint> : null}
     </Container>
   );
 };
